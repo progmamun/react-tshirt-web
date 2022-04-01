724802e66c03001a3d30abf58fec8aa385503d3d
progmamun <mamun333089@gmail.com>
2022-04-01 11:27:20 +0600

    load-t-shirt-data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Home.css';
 import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
+import useTShirts from '../../hooks/useTShirts';
 
 const Home = () => {
+  const [tShirts, setTShirts] = useTShirts();
   return (
     <div className="home-container">
-      <div className="tshirt-container"></div>
+      <div className="tshirt-container">
+        {tShirts.map(tShirt => (
+          <TShirt key={tShirt._id} tShirt={tShirt}></TShirt>
+        ))}
+      </div>
       <div className="cart-container">
         <Cart></Cart>
       </div>

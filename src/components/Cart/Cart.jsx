fb658c393407ae6bc588dcc3645c2c3c2c8c1943
progmamun <mamun333089@gmail.com>
2022-04-01 13:58:18 +0600

    element-variable

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+  // Conditional Rendering Options
+  // 1. Element Variable
+  let command;
+  if (cart.length === 0) {
+    command = <p>Please Add at least one items</p>;
+  } else if (cart.length === 1) {
+    command = <p>Please add more...</p>;
+  } else {
+    command = (
+      <p>
+        <small>Thanks for adding item</small>
+      </p>
+    );
+  }
   return (
     <div>
       <h2>Items Selected: {cart.length}</h2>
@@ -11,6 +25,7 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
           <button onClick={() => handleRemoveFromCart(tShirt)}>X</button>
         </p>
       ))}
+      {command}
     </div>
   );
 };

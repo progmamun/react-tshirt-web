5e05a42eb7d5aae9be6d9696833bb25a3d296928
progmamun <mamun333089@gmail.com>
2022-04-01 12:14:48 +0600

    remove-from-cart

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
@@ -6,15 +6,34 @@ import useTShirts from '../../hooks/useTShirts';
 
 const Home = () => {
   const [tShirts, setTShirts] = useTShirts();
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = selectedItem => {
+    const exists = cart.find(tShirt => tShirt._id === selectedItem._id);
+    if (!exists) {
+      const newCart = [...cart, selectedItem];
+      setCart(newCart);
+    } else {
+      alert('item already added');
+    }
+  };
+  const handleRemoveFromCart = selectedItem => {
+    const rest = cart.filter(tShirt => tShirt._id !== selectedItem._id);
+    setCart(rest);
+  };
   return (
     <div className="home-container">
       <div className="tshirt-container">
         {tShirts.map(tShirt => (
-          <TShirt key={tShirt._id} tShirt={tShirt}></TShirt>
+          <TShirt
+            key={tShirt._id}
+            tShirt={tShirt}
+            handleAddToCart={handleAddToCart}
+          ></TShirt>
         ))}
       </div>
       <div className="cart-container">
-        <Cart></Cart>
+        <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
       </div>
     </div>
   );
